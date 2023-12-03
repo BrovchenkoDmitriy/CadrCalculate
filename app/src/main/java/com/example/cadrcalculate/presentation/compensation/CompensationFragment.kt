e2e78e3ab7d3711ce0b27eb4efa66d9232a39a9c
BrovchenkoDmitriy <99586855+BrovchenkoDmitriy@users.noreply.github.com>
2023-12-03 20:27:39 +0900

    UI Task  - complete logic, ui.

--- app/src/main/java/com/example/cadrcalculate/presentation/compensation/CompensationFragment.kt
+++ app/src/main/java/com/example/cadrcalculate/presentation/compensation/CompensationFragment.kt
@@ -1,11 +1,19 @@
 package com.example.cadrcalculate.presentation.compensation
 
+import android.app.DatePickerDialog
+import android.os.Build
 import android.os.Bundle
+import android.text.format.DateFormat
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.annotation.RequiresApi
+import androidx.core.widget.addTextChangedListener
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.example.cadrcalculate.databinding.FragmentCompensationBinding
+import java.time.LocalDate
+import java.util.Calendar
 
 class CompensationFragment : Fragment() {
 
@@ -13,6 +21,9 @@ class CompensationFragment : Fragment() {
     private val binding: FragmentCompensationBinding
         get() = _binding ?: throw RuntimeException("FragmentCompensationBinding is null")
 
+    private val viewModel by lazy {
+        ViewModelProvider(this)[CompensationViewModel::class.java]
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -23,9 +34,89 @@ class CompensationFragment : Fragment() {
         return binding.root
     }
 
+    @RequiresApi(Build.VERSION_CODES.O)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.startDay.observe(viewLifecycleOwner) {
+            with(binding) {
+                val dateFormat = DateFormat.format("\"dd MMMM yyyy\"", getCalendarFromLocalDate(it))
+                tvBeginWork.text = dateFormat
+            }
+        }
+        viewModel.endDay.observe(viewLifecycleOwner) {
+            with(binding) {
+                val dateFormat = DateFormat.format("\"dd MMMM yyyy\"", getCalendarFromLocalDate(it))
+                tvEndWork.text = dateFormat
+            }
+        }
+        viewModel.answer.observe(viewLifecycleOwner) {
+            binding.tvResult.text = it
+        }
+
+        val startDate = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            val startCalendar = Calendar.getInstance()
+            startCalendar.set(Calendar.YEAR, year)
+            startCalendar.set(Calendar.MONTH, month)
+            startCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            viewModel.setStartDay(LocalDate.of(year, month + 1, dayOfMonth))
+        }
+
+        binding.ivBeginWork.setOnClickListener {
+            DatePickerDialog(requireContext(), startDate, 2020, 0, 1).show()
+        }
+
+        val endDate = DatePickerDialog.OnDateSetListener { _, year, month, dayOfMonth ->
+            val endCalendar = Calendar.getInstance()
+            endCalendar.set(Calendar.YEAR, year)
+            endCalendar.set(Calendar.MONTH, month)
+            endCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth)
+            viewModel.setEndDay(LocalDate.of(year, month + 1, dayOfMonth))
+        }
+
+        binding.ivEndWork.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            DatePickerDialog(
+                requireContext(),
+                endDate,
+                calendar.get(Calendar.YEAR),
+                calendar.get(Calendar.MONTH),
+                calendar.get(Calendar.DAY_OF_MONTH)
+            )
+                .show()
+        }
+
+        binding.checkBox.setOnCheckedChangeListener { buttonView, _ ->
+            if (buttonView.isChecked) {
+                binding.editTextText.visibility = View.VISIBLE
+            } else {
+                binding.editTextText.visibility = View.GONE
+            }
+        }
+
+        binding.editTextText.addTextChangedListener {
+            it?.let {
+                if (it.isNotEmpty() && !it.contains("[^0-9]")) {
+                    viewModel.setHolidays(it.toString().toInt())
+                } else {
+                    viewModel.setHolidays(0)
+                }
+            }
+        }
+
+        binding.button2.setOnClickListener {
+            viewModel.calculateCompensation()
+        }
+    }
+
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
     }
+}
 
+@RequiresApi(Build.VERSION_CODES.O)
+private fun getCalendarFromLocalDate(localDate: LocalDate): Calendar {
+    val calendar = Calendar.getInstance()
+    calendar.set(localDate.year, localDate.monthValue - 1, localDate.dayOfMonth)
+    return calendar
 }
---
+++ app/src/main/java/com/example/cadrcalculate/presentation/compensation/CompensationViewModel.kt
@@ -0,0 +1,95 @@
+package com.example.cadrcalculate.presentation.compensation
+
+import android.os.Build
+import androidx.annotation.RequiresApi
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.time.LocalDate
+import java.time.Period
+
+class CompensationViewModel : ViewModel() {
+    private val _startDay = MutableLiveData<LocalDate>()
+    val startDay: LiveData<LocalDate>
+        get() = _startDay
+    private val _endDay = MutableLiveData<LocalDate>()
+    val endDay: LiveData<LocalDate>
+        get() = _endDay
+    private val _holidays = MutableLiveData<Int>()
+    private val holidays: LiveData<Int>
+        get() = _holidays
+
+    private val _answer = MutableLiveData<String>()
+    val answer: LiveData<String>
+        get() = _answer
+
+    fun setStartDay(startDay: LocalDate) {
+        _startDay.value = startDay
+    }
+
+    fun setEndDay(endDay: LocalDate) {
+        _endDay.value = endDay
+    }
+
+    fun setHolidays(holidays: Int) {
+        _holidays.value = holidays
+    }
+
+    @RequiresApi(Build.VERSION_CODES.O)
+    fun calculateCompensation() {
+
+        if (startDay.value == null || endDay.value == null) {
+            _answer.value = "Укажите даты приёма на работу и увольнения."
+        } else {
+            val amountOfMonth = (holidays.value ?: 0) / 3
+            val endWithCorrect = endDay.value?.minusMonths(amountOfMonth.toLong())
+            val period = Period.between(startDay.value, endWithCorrect)
+            if (period.years < 0) {
+                _answer.value = "Некорректно указаны даты приёма на работу и увольнения."
+            } else {
+                _answer.value =
+                    "${period.years}  ${correctYear(period.years)}," +
+                            "\n${period.months} ${correctMonth(period.months)}," +
+                            "\n${period.days} ${correctDays(period.days)}"
+            }
+        }
+    }
+}
+
+private fun correctYear(int: Int): String {
+    return when (int) {
+        0 -> "лет"
+        1 -> "год"
+        in 2..4 -> "года"
+        in 5..20 -> "лет"
+        in 21..24 -> "года"
+        in 25..30 -> "лет"
+        in 31..34 -> "года"
+        in 35..40 -> "лет"
+        in 41..44 -> "года"
+        in 45..50 -> "лет"
+        else -> "моё почтение"
+    }
+}
+
+private fun correctMonth(int: Int): String {
+    return when (int) {
+        0, in 5..12 -> "месяцев"
+        1 -> "месяц"
+        2, 3, 4 -> "месяца"
+        else -> ""
+    }
+}
+
+private fun correctDays(int: Int): String {
+    return when (int) {
+        0 -> "дней"
+        1, 21, 31 -> "день"
+        in 2..4 -> "дня"
+        in 5..20 -> "дней"
+        in 22..24 -> "дня"
+        in 25..30 -> "дней"
+        else -> ""
+    }
+}
+
